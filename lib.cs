df5db1f92df3141a1896f5cc96f26c10b680ccc7
volhayakauleva <52138253+volhayakauleva@users.noreply.github.com>
2020-05-26 21:25:35 +0300

    f

---
+++ lib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class lib
+    {
+        public static void ShowSystemInfo()
+        {
+            Console.WriteLine("Сведения о системе:");
+            Console.WriteLine("\tОперационная система:  {0}", Environment.OSVersion);
+            Console.WriteLine("\tИмя пользователя: {0}", Environment.UserName);
+            Console.WriteLine("\tНазвание ПК по биосу:  {0}", Environment.MachineName);
+            Console.WriteLine("\tВремя с первой загрузки ПК (сек):  {0}", Environment.TickCount / 1000);
+            Console.WriteLine("\tИмя сетевого домена:  {0}", Environment.UserDomainName);
+            Console.WriteLine();
+
+            Console.WriteLine("Сведения о Процессоре:");
+            Console.ResetColor();
+            Console.WriteLine("\tМодель процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER"));
+            Console.WriteLine("\tРазрядность процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+            Console.WriteLine("\tНомер модели процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_LEVEL"));
+            Console.WriteLine("\tЧисло процессоров:  {0}", Environment.ProcessorCount);
+        }
+    }
+}
